dc72ff9bf3f7c4d2b32c8cc5b0b3b03b8c25aac6
Olucho <luchoorfino@gmail.com>
2021-02-22 23:31:20 -0300

    Injects Service and Repository in module

--- backend/src/car/car.controller.ts
+++ backend/src/car/car.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Post } from '@nestjs/common';
+import { CarService } from './car.service';
+import { CreateCarDto } from './dto/create.car.dto';
 @Controller('car')
-export class CarController {}
+export class CarController {
+  constructor(private carService: CarService) {}
+
+  @Post()
+  createCar(@Body() createCarDto: CreateCarDto) {
+    return this.carService.createCar(createCarDto);
+  }
+}
--- backend/src/car/car.module.ts
+++ backend/src/car/car.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CarController } from './car.controller';
+import { CarRepository } from './car.repository';
 import { CarService } from './car.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([CarRepository])],
   controllers: [CarController],
   providers: [CarService],
 })
