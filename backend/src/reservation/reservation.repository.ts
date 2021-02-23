8e757449ae35609604d3d43a1926d093adb3c1ba
Olucho <luchoorfino@gmail.com>
2021-02-23 13:34:20 -0300

    Add Repository

--- backend/src/reservation/reservation.repository.ts
+++ backend/src/reservation/reservation.repository.ts
@@ -1,9 +1,34 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { CreateReservationDto } from './dto/create.Reservation.dto';
+import { Reservation } from './reservation.entity';
+
 @EntityRepository(Reservation)
 export class ReservationRepository extends Repository<Reservation> {
-  async createReservation() {}
+  async createReservation(
+    createReservationDto: CreateReservationDto,
+  ): Promise<Reservation> {
+    const {
+      finishDate,
+      paymentMethod,
+      pricePerDay,
+      startDate,
+      status,
+      totalPrice,
+    } = createReservationDto;
 
-  async getAllReservation(): Promise<Reservation[]> {
+    const reservation = new Reservation();
+    reservation.finishDate = finishDate;
+    reservation.paymentMethod = paymentMethod;
+    reservation.pricePerDay = pricePerDay;
+    reservation.startDate = startDate;
+    reservation.status = status;
+    reservation.totalPrice = totalPrice;
+
+    await reservation.save();
+    return reservation;
+  }
+
+  async getAllReservations(): Promise<Reservation[]> {
     const query = this.createQueryBuilder('Reservation');
     return query.getMany();
   }
