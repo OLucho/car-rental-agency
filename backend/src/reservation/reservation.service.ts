5df8393beb4be9e4d42fd7432146a2a855756bff
Olucho <luchoorfino@gmail.com>
2021-02-23 13:28:28 -0300

    Add Service methods

--- backend/src/reservation/reservation.service.ts
+++ backend/src/reservation/reservation.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateReservationDto } from './dto/create.reservation.dto';
+import { Reservation } from './reservation.entity';
+import { ReservationRepository } from './reservation.repository';
 
 @Injectable()
-export class ReservationService {}
+export class ReservationService {
+  constructor(
+    @InjectRepository(ReservationRepository)
+    private reservationRepository: ReservationRepository,
+  ) {}
+
+  async createReservation(
+    createReservationDto: CreateReservationDto,
+  ): Promise<Reservation> {
+    return await this.reservationRepository.createReservation(
+      createReservationDto,
+    );
+  }
+
+  async getAllReservations(): Promise<Reservation[]> {
+    return await this.reservationRepository.getAllReservations();
+  }
+  async getReservationById(id: number): Promise<Reservation> {
+    const Reservation = await this.reservationRepository.findOne({
+      where: { id },
+    });
+
+    if (!Reservation) {
+      throw new NotFoundException('Reservation not found');
+    }
+
+    return Reservation;
+  }
+
+  async deleteReservationById(id: number) {
+    const result = await this.reservationRepository.delete(id);
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+  }
+}
