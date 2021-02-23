10a7a7547d6b955dfabd775b215009efc6764458
Olucho <luchoorfino@gmail.com>
2021-02-23 13:34:13 -0300

    Injects Reservation Repository in module

--- backend/src/reservation/reservation.module.ts
+++ backend/src/reservation/reservation.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReservationController } from './reservation.controller';
+import { ReservationRepository } from './reservation.repository';
 import { ReservationService } from './reservation.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ReservationRepository])],
   controllers: [ReservationController],
   providers: [ReservationService],
 })
