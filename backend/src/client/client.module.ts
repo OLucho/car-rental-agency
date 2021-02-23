7861b97f3ae2854ad17206541e7c2ac03203da16
Olucho <luchoorfino@gmail.com>
2021-02-23 11:00:57 -0300

    Add Client Repository and injects in module

--- backend/src/client/client.module.ts
+++ backend/src/client/client.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClientController } from './client.controller';
+import { ClientRepository } from './client.repository';
 import { ClientService } from './client.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ClientRepository])],
   controllers: [ClientController],
   providers: [ClientService],
 })
