9495d6143772edc9a94d387fdbdff56d62aef5ca
Olucho <luchoorfino@gmail.com>
2021-02-25 11:10:55 -0300

    Add MUI Menu component in table instead of a single button

--- frontend/src/components/table/index.js
+++ frontend/src/components/table/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable array-callback-return */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -9,7 +9,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { Button } from '@material-ui/core';
+import { Button, Menu, MenuItem } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles({
@@ -17,13 +17,32 @@ const useStyles = makeStyles({
     minWidth: 650,
   },
   button: {
-    background: 'blue',
+    textAlign: 'center',
+    backgroundColor: 'cadetblue',
+    color: 'white',
+  },
+  anchor: {
+    textDecoration: 'none',
     color: 'white',
   },
 });
 
 export default function BasicTable({ thData, tbData, type }) {
   const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleDelete = (type, id) => {
+    console.log(type, id);
+  };
 
   return (
     <TableContainer component={Paper}>
@@ -43,11 +62,36 @@ export default function BasicTable({ thData, tbData, type }) {
                   {el}
                 </TableCell>
               ))}
-              <Link to={`/${type}/${row[0]}`}>
-                <Button size="small" className={classes.button} id={row[0]}>
-                  Details
+
+              <div className={classes.button}>
+                <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                  Actions
                 </Button>
-              </Link>
+                <Menu
+                  id="simple-menu"
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>
+                    <Button variant="contained" color="primary">
+                      <Link to={`/${type}/${row[0]}`} className={classes.anchor}>
+                        See Details
+                      </Link>
+                    </Button>
+                  </MenuItem>
+                  <MenuItem>
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      onClick={() => handleDelete(type, row[0])}
+                    >
+                      Delete
+                    </Button>
+                  </MenuItem>
+                </Menu>
+              </div>
             </TableRow>
           ))}
         </TableBody>
