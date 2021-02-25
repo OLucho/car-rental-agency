42f7db960a99536aaa828ae9cb9e7329dc7d5656
Olucho <luchoorfino@gmail.com>
2021-02-25 17:52:37 -0300

    Add car detail page

--- frontend/src/pages/detailPage/index.js
+++ frontend/src/pages/detailPage/index.js
@@ -1,38 +1,14 @@
-import { makeStyles } from '@material-ui/core';
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import CarDetail from '../../components/detail/carDetail';
 import Header from '../../components/header';
-import { useCar } from '../../hooks/useCar';
 
-const useStyles = makeStyles({
-  title: {
-    textAlign: 'center',
-  },
-  alert: {
-    margin: '1rem 0',
-  },
-});
 export default function DetailPage() {
   const { type, id } = useParams();
-  const classes = useStyles();
 
-  const [data, setData] = useState('');
-  const { car, getCarById } = useCar();
-
-  useEffect(() => {
-    if (type === 'car') {
-      getCarById(id);
-      setData(car);
-    }
-  }, []);
   return (
     <>
       <Header />
-      <div className={classes.title}>
-        <h1>
-          Viewing {type} with ID {id}
-        </h1>
-      </div>
+      {type === 'car' && <CarDetail id={id} />}
     </>
   );
 }
