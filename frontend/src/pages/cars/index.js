cb277169514357079d9a125ef8014ddb63f67871
Olucho <luchoorfino@gmail.com>
2021-02-25 22:50:35 -0300

    Shows errors in car page if any

--- frontend/src/pages/cars/index.js
+++ frontend/src/pages/cars/index.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable array-callback-return */
 import { makeStyles } from '@material-ui/core';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Alert } from '@material-ui/lab';
 import Header from '../../components/header';
 import Table from '../../components/table';
 import { useCar } from '../../hooks/useCar';
 import CarForm from '../../components/form/carForm';
+import AlertError from '../../components/error/alertError';
 
 const thRow = [
   'ID',
@@ -34,7 +35,7 @@ const useStyles = makeStyles({
 export default function CarsPage() {
   const classes = useStyles();
 
-  const { getAllCars, cars, status, setStatus } = useCar();
+  const { getAllCars, cars, status, setStatus, error } = useCar();
   useEffect(() => {
     getAllCars();
   }, [getAllCars, setStatus, status]);
@@ -51,6 +52,7 @@ export default function CarsPage() {
             {status}
           </Alert>
         )}
+        {error && <AlertError error={error} />}
         <CarForm />
       </div>
       {cars.length > 0 && <Table thData={thRow} tbData={tbData} type="car" />}
