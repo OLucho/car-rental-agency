1fccf89831836506bd1012c2a112e95559674d76
Olucho <luchoorfino@gmail.com>
2021-03-04 17:36:48 -0300

    add tests

--- frontend/src/pages/cars/index.spec.js
+++ frontend/src/pages/cars/index.spec.js
@@ -10,35 +10,48 @@ import Providers from '../../hooks/providers';
 const carsMock = [
   {
     air_conditioning: 'no',
-    brand: 'asdasd',
-    color: '23123',
+    brand: 'brand1',
+    color: 'red',
     id: 1,
     image: 'In construction',
-    kms: '123213',
-    model: 'asdasd',
-    passengers: 123123,
-    price: 12321321,
-    year: 21321,
+    kms: '2',
+    model: 'model1',
+    passengers: 1,
+    price: 1,
+    year: 1,
   },
   {
-    air_conditioning: 'no',
-    brand: 'asdasd',
-    color: '23123',
-    id: 1,
+    air_conditioning: 'yes',
+    brand: 'brand2',
+    color: 'blue',
+    id: 2,
     image: 'In construction',
-    kms: '123213',
-    model: 'asdasd',
-    passengers: 123123,
-    price: 12321321,
-    year: 21321,
+    kms: '1',
+    model: 'model2',
+    passengers: 2,
+    price: 2,
+    year: 2,
   },
 ];
 
-const getCarsMock = jest.fn();
+const thRow = [
+  'ID',
+  'Brand',
+  'Model',
+  'Year',
+  'Kms',
+  'Color',
+  'Amount Of Passengers',
+  'price',
+  'image',
+  'Has Air Conditioning?',
+];
+
+const mockResponseApi = jest.fn();
 
 const server = setupServer(
   rest.get('http://localhost:3000/car', (req, res, ctx) => {
-    const result = getCarsMock();
+    const result = mockResponseApi();
     return res(ctx.json(result || []));
   })
 );
@@ -49,7 +62,7 @@ afterAll(() => server.close());
 
 describe('<CarsPage/>', () => {
   it('Renders table if there are at least one car', async () => {
-    getCarsMock.mockReturnValueOnce(carsMock);
+    mockResponseApi.mockReturnValueOnce(carsMock);
     const component = render(
       <Router>
         <Providers>
@@ -57,5 +70,32 @@ describe('<CarsPage/>', () => {
         </Providers>
       </Router>
     );
+
+    await component.findByText('There are 2 cars');
+
+    for (let i = 0; i < thRow.length; i++) {
+      await component.findByText(thRow[i]);
+    }
+
+    for (let i = 0; i < carsMock.length; i++) {
+      const tbData = Object.values(carsMock[i]);
+      await component.findByText(tbData[i]);
+    }
+  });
+
+  it('Do not render table if there is no cars', async () => {
+    mockResponseApi.mockReturnValueOnce([]);
+    const component = render(
+      <Router>
+        <Providers>
+          <CarsPage />
+        </Providers>
+      </Router>
+    );
+    await component.findByText('There are 0 cars');
+    await component.findByText(/Create new car!/i);
+
+    const table = screen.queryByRole('table');
+    expect(table).toBeNull();
   });
 });
