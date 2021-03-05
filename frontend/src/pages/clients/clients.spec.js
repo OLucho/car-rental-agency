98621c258b12bf2522fcf741cc43787b1844142e
Olucho <luchoorfino@gmail.com>
2021-03-05 00:44:18 -0300

    Add RTL tests

--- frontend/src/pages/clients/clients.spec.js
+++ frontend/src/pages/clients/clients.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
-import { findByText, render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ClientsPage from './index';
